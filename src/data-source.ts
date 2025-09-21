68ab5fa15dec21d30e695af686aba536bd397b37
yukesh570 <yukesh.maha10@gmail.com>
2025-09-22 01:15:46 +0545

    changes

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,24 +1,31 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+dotenv.config({path:"./.env"});
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT),
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: false,
-  logging: false,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'] ,
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: false, 
+    logging: false,
+      entities:
+    process.env.ENV === "local"
+      ? ["src/entity/**/*.ts"]
+      : ["dist/entity/**/*.js"],
+      // : ["dist/src/entity/**/*.js"],
 
-migrations: process.env.ENV === "local"
-  ? ["src/migration/**/*.ts"]
-  : ["dist/migration/**/*.js"],
-  // : ["dist/src/migration/*.js"],
-  ssl: {
+  // entities: ["src/entity/**/*.ts"],  // Target compiled JavaScript files
+  migrations:
+    process.env.ENV === "local"
+      ? ["src/migration/*.ts"]
+      : ["dist/migration/*.js"],
+      // : ["dist/src/migration/*.js"],
+      ssl: {
     rejectUnauthorized: false,
   },
 });
+
