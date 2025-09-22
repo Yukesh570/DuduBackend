4ea83d864fc8fe02c9b0d028c855b1630ff564be
yukesh570 <yukesh.maha10@gmail.com>
2025-09-22 13:18:58 +0545

    changse

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,27 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
-import { User } from "./entity/users/user";
-dotenv.config({path:"./.env"});
+
+dotenv.config({ path: "./.env" });
+
+const isProd = process.env.NODE_ENV === "production";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: false, 
-    logging: false,
-    entities: [__dirname + "/entity/**/*{.ts,.js}"],
-    migrations: [__dirname + "/migration/**/*{.ts,.js}"],
-
-      ssl: {
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432", 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: false,
+  entities: isProd
+    ? ["dist/entity/**/*{.js,.ts}"]
+    : ["src/entity/**/*{.ts,.js}"],
+  migrations: isProd
+    ? ["dist/migration/**/*{.js,.ts}"]
+    : ["src/migration/**/*{.ts,.js}"],
+  ssl: {
     rejectUnauthorized: false,
   },
 });
-
