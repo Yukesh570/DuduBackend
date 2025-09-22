7bca0d0410c52e85089b7f06c0dbf098966b90ef
yukesh570 <yukesh.maha10@gmail.com>
2025-09-22 11:45:14 +0545

    changes

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,32 +1,27 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
-import { User } from "./entity/users/user";
-dotenv.config({path:"./.env"});
+
+dotenv.config({ path: "./.env" });
+
+const isLocal = process.env.ENV === "local";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.POSTGRES_PORT),
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    synchronize: false, 
-    logging: false,
-      entities:[User],
-    // process.env.ENV === "local"
-    //   ? ["src/entity/**/*.ts"]
-    //   : ["dist/entity/**/*.js"],
-      // : ["dist/src/entity/**/*.js"],
-
-  // entities: ["src/entity/**/*.ts"],  // Target compiled JavaScript files
-  migrations:
-    process.env.ENV === "local"
-      ? ["src/migration/*.ts"]
-      : ["dist/migration/*.js"],
-      // : ["dist/src/migration/*.js"],
-      ssl: {
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  logging: false,
+  entities: isLocal
+    ? ["src/entity/**/*.ts"]
+    : ["dist/entity/**/*.js"],
+  migrations: isLocal
+    ? ["src/migration/*.ts"]
+    : ["dist/migration/*.js"],
+  ssl: {
     rejectUnauthorized: false,
   },
 });
-
